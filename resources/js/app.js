c741570afa52bf1a31bcb3c60f93a4f9851c3664
William Go <gilberthoop@Williams-MacBook-Pro.local>
2022-03-15 23:32:58 -0700

    Add Vuetify plugin for styling

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,16 @@ require('./bootstrap');
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuetify from '../plugins/vuetify'
 import router from './routes'
 import Index from './index'
 
 Vue.use(VueRouter)
+Vue.use(Vuetify)
 
 const app = new Vue({
     el: '#app',
+    vuetify: Vuetify,
     router,
     components: {
       'index': Index
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 
     <title>True North Conference | SFC Canada</title>
 
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
     <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
