bc119abbedf571e4df6464a5d5217e7691861457
William Go <gilberthoop@Williams-MacBook-Pro.local>
2022-03-20 15:52:44 -0700

    Install packages Vuex VeeValidate and Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,10 @@ require('./bootstrap');
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Vuetify from '../plugins/vuetify'
+import VeeValidate from 'vee-validate'
+
 import router from './routes'
+import store from './store'
 import Index from './index'
 
 Vue.use(VueRouter)
@@ -12,7 +15,9 @@ Vue.use(Vuetify)
 const app = new Vue({
     el: '#app',
     vuetify: Vuetify,
+    VeeValidate,
     router,
+    store,
     components: {
       'index': Index
     }
