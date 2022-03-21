5999348580e1c3c549cb66cf646527f6fbc93c15
William Go <gilberthoop@Williams-MacBook-Pro.local>
2022-03-20 23:14:37 -0700

    Display a list of participants

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,7 @@
 import VueRouter from 'vue-router'
 import ExampleComponent from './components/ExampleComponent'
 import Register from './auth/Register'
+import Participants from './pages/Participants'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/register',
     component: Register,
     name: 'register'
+  },
+  {
+    path: '/participants',
+    component: Participants,
+    name: 'participants'
   }
 ]
 
